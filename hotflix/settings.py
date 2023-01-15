3fce642c00a71fe2bc2e81405b48b2ff196b1041
rajababu027 <rajababu12271998@gmail.com>
2023-01-15 08:55:54 +0530

    se

--- hotflix/settings.py
+++ hotflix/settings.py
@@ -132,4 +132,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
